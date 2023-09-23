1c1963ac0ca6e32880b09e0edfe935bc96400ee0
Volodymyr But <vbut.dev+github@gmail.com>
2023-09-23 19:15:50 +0200

    feat: add telescope.nvim

---
+++ after/plugin/telescope.rc.lua
@@ -0,0 +1,32 @@
+local telescope_status, telescope = pcall(require, "telescope")
+if not telescope_status then
+    return
+end
+
+telescope.setup({
+    defaults = {
+        prompt_prefix = "> ",
+        selection_caret = "> ",
+        layout_strategy = "horizontal",
+        sorting_strategy = "ascending",
+        selection_strategy = "closest",
+        path_display = { "truncate" },
+        borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+        layout_config = { horizontal = { prompt_position = "top" } },
+        initial_mode = "insert",
+    },
+    pickers = {
+        find_files = {
+            respect_gitignore = true,
+            disable_devicons = true,
+            hidden = false,
+        },
+    },
+})
+
+local builtin_status, builtin = pcall(require, "telescope.builtin")
+if builtin_status then
+    local nvim = require("byut.utils.nvim")
+    nvim.keymap.set("<space>ff", builtin.find_files)
+    nvim.keymap.set("<space>fd", builtin.lsp_document_symbols)
+end
--- lua/byut/lazy/plugins.lua
+++ lua/byut/lazy/plugins.lua
@@ -6,4 +6,8 @@ return {
     "nvim-treesitter/nvim-treesitter", -- code parser that provides folding, highlighting, and more
     "windwp/nvim-autopairs", -- auto-close parentheses, brackets, quotes, and more
     "windwp/nvim-ts-autotag", -- auto-close and auto-rename html(jsx) tags
+
+    ---
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim",
 }
